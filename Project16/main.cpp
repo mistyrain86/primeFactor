c1ec07823fbf44cf9ba30979180a36cb22aa9a0f
Choi <mistyrain86@gmail.com>
2026-05-28 09:07:35 +0900

    [feature] page 29

--- Project16/main.cpp
+++ Project16/main.cpp
@@ -17,6 +17,10 @@ TEST_F(PrimeFixture, Of2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+TEST_F(PrimeFixture, Of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
 
 int main()
 {
